c25907061afdc3a3f45e888e1e1029220de95510
profniltonbarros <prof.niltonbarros@gmail.com>
2026-05-09 10:40:42 -0300

    Python Entradas

---
+++ 03_python_entradas/entradas.py
@@ -0,0 +1,10 @@
+# Autor: Nilton Barros
+# Projeto: Estudando entradas e saídas em Python
+
+# declaração de variáveis
+valor1 = 10
+valor2 = 20
+resultado = valor1 + valor2
+
+# exibindo resultados
+print(resultado)
