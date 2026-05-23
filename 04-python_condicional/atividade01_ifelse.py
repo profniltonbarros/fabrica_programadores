8f44483ae4ad7da143902b62e59be1a8d3559fd1
profniltonbarros <prof.niltonbarros@gmail.com>
2026-05-23 08:11:22 -0300

    Condicionais

---
+++ 04-python_condicional/atividade01_ifelse.py
@@ -0,0 +1,10 @@
+# Autor: Nilton Barros
+# Projeto: Condicionais
+
+# Definição das variáveis
+nome = input('Digite seu nome: ')
+nota = float(input('Digite sua nota: '))
+if nota >= 6:
+    print('Aluno Aprovado!')
+else:
+    print('Aluno Reprovado!')
